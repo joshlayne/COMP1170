24cdb46a2e1787161aa7b045929112a65b39fd29
joshlayne <73448488+joshlayne@users.noreply.github.com>
2022-10-30 23:54:14 -0400

    lab 3 javascript

---
+++ Lab 3/external/external.js
@@ -0,0 +1,8 @@
+function times() {
+    var num1 = prompt("Please enter a number: ", "0");
+    var num2 = prompt("Please enter another number: ", "0");
+
+    var product = num1 * num2;
+
+    document.write("<h1> The multiple is: <h1>" + product);
+}
